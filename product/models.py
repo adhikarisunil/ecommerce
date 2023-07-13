e44ae3723da428f5f163d94c4eb220cbf051cafc
sunil <adhikarisunil339@gmail.com>
2023-07-13 17:23:42 +0545

    update

--- product/models.py
+++ product/models.py
@@ -2,26 +2,29 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 # Create your models here.
-class Category(models.Model):
+class Category(MPTTModel):
     name = models.CharField(max_length=255)
-    # category_name = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
 
-    # class MPTTMeta:
-    #     order_insertion_by = ["name"]
-    # def __str__(self):
-        # return self.name
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+    def __str__(self):
+        return self.name
 
 class Brand(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    # description = models.TextField(blank=True)
-    # is_digital = models.BooleanField(default=False)
-    # brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    # category = TreeForeignKey('Category', on_delete=models.SET, null=True, blank=True)
+    description = models.TextField(blank=True)
+    is_digital = models.BooleanField(default=False)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category = TreeForeignKey('Category', on_delete=models.SET, null=True, blank=True)
 
 
-    # def __str__(self):
-    #     return self.name
+    def __str__(self):
+        return self.name
 
