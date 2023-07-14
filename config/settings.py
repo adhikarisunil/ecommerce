5e09f71bdb25e1770846e63b3864b779cd438744
sunil <adhikarisunil339@gmail.com>
2023-07-14 11:18:14 +0545

    update

--- config/settings.py
+++ config/settings.py
@@ -59,8 +59,14 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
 }
 
 TEMPLATES = [
